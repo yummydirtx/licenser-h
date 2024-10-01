fe289d12affd39423d20c0069e3ccf0ea14285f3
Alex Frutkin <frutkinalexander@gmail.com>
2024-09-30 19:05:56 -0700

    fix bug where apache2 license isn't found

--- src/full_licenses.py
+++ src/full_licenses.py
@@ -765,7 +765,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.""",
-    "apache": """                                 Apache License
+    "apache2": """                                 Apache License
                            Version 2.0, January 2004
                         http://www.apache.org/licenses/
 
