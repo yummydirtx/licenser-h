fce612a4b0c746922f6d6ce9953e823c4af1eb0e
Alex Frutkin <61663391+yummydirtx@users.noreply.github.com>
2025-09-18 05:50:10 +0000

    Update copyright year from 2024 to 2025 in all relevant files

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Alex Frutkin
+# Copyright (c) 2025 Alex Frutkin
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (Licenser), to deal in
