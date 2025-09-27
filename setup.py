770a12252d0cadef8f9f9756ab7d2f0b99d96c06
Alex Frutkin <frutkinalexander@gmail.com>
2025-09-26 19:30:51 -0700

    Add GitHub Actions workflow for PyPI publishing

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='licenser-h',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     install_requires=[
         'argparse',
